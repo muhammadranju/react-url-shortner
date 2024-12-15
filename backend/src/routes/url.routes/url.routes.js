488507ae7cf343e4f6ccb6b6d0ec10b3910a52aa
Muhammad Ranju <mdranju23@gmail.com>
2024-12-15 15:29:40 +0600

    feat: Update the url routes and add authMiddleware

--- backend/src/routes/url.routes/url.routes.js
+++ backend/src/routes/url.routes/url.routes.js
@@ -5,8 +5,10 @@ const authMiddleware = require("../../middleware/authMiddleware");
 
 router.route("/v1/api/short-urls").get(authMiddleware, urlController.findAll);
 router.route("/v1/api/short-urls").post(authMiddleware, urlController.create);
-router.route("/v1/api/short-urls/:id").get(urlController.findOne);
-router.route("/v1/api/short-urls/:id").patch(urlController.update);
-router.route("/v1/api/short-urls/:id").delete(urlController.deleteShortUrl);
+router
+  .route("/v1/api/short-urls/:id")
+  .get(authMiddleware, urlController.findOne);
+// router.route("/v1/api/short-urls/:id").patch(urlController.update);
+// router.route("/v1/api/short-urls/:id").delete(urlController.deleteShortUrl);
 
 module.exports = router;
