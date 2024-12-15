d305c8d1cbb8b813356f5218ab4c925b2e0d8094
Muhammad Ranju <mdranju23@gmail.com>
2024-12-15 15:27:56 +0600

    feat: create Short URL functionality and implementation

--- backend/src/api/v1/url/controllers/create.js
+++ backend/src/api/v1/url/controllers/create.js
@@ -1,11 +1,77 @@
+const moment = require("moment/moment");
 const ShortURL = require("../../../../models/url.model/url.model");
+const shortid = require("shortid");
 
 const create = async (req, res) => {
   try {
-    const { originalUrl } = req.body;
-    console.log(req.user);
-    console.log(originalUrl);
-    res.status(201).json(originalUrl);
+    const URL_REGEX =
+      /^(https?:\/\/)?((([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})|localhost)(:[0-9]{1,5})?(\/[^\s]*)?$/;
+    const MAX_LINKS = 10;
+
+    let { originalUrl, icon } = req.body;
+
+    if (!originalUrl) {
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: "Please provide a valid URL",
+      });
+    }
+
+    // Remove trailing slash if it exists
+    originalUrl = originalUrl.replace(/\/+$/, "");
+
+    const allLinks = await ShortURL.find().sort({ _id: -1 }).limit(MAX_LINKS);
+
+    if (!URL_REGEX.test(originalUrl.trim())) {
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: "Please enter a valid URL",
+      });
+    }
+
+    const formattedCreatedAt = moment(allLinks.createdAt).format(
+      "DD-MM-YYYY, HH:mm:ss"
+    );
+
+    const existingURL = await ShortURL.findOne({ originalUrl });
+
+    if (existingURL) {
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: "You can't shorten the same URL again",
+        existingLink: {
+          url: existingURL.shortUrl,
+          success: true,
+        },
+      });
+    }
+
+    const shortLink = shortid.generate().toLowerCase();
+    const newLongURL = `https://${req.hostname}/${shortLink}`;
+
+    const links = new ShortURL({
+      originalUrl,
+      shotLink: shortLink,
+      shortUrl: newLongURL,
+      dateTime: formattedCreatedAt,
+      icon,
+      user: req.user.id,
+    });
+    await links.save();
+
+    console.log(links);
+
+    return res.status(201).json({
+      status: 201,
+      success: true,
+      allLinks,
+      newLongURL,
+      links,
+      message: "Successfully created",
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error creating short URL" });
