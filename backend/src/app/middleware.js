bb4759dc8819bb17174d3bdda6819b69b70e3fad
Muhammad Ranju <mdranju23@gmail.com>
2024-12-16 11:46:00 +0600

    feat: update middleware modify cors function

--- backend/src/app/middleware.js
+++ backend/src/app/middleware.js
@@ -6,11 +6,12 @@ const morgan = require("morgan");
 const routes = require("../routes/index");
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL, // Allow requests only from this frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // If you're using cookies or sessions
+  origin: process.env.FRONTEND_URL, // Frontend origin
+  credentials: true, // Allow credentials (cookies)
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], // Add required headers
 };
+
 const middleware = [
   express.json(),
   cookieParser(),
