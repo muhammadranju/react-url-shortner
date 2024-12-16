45c85e43d29b06f9e7c2940714e4ddf9a35fbf71
Muhammad Ranju <mdranju23@gmail.com>
2024-12-16 11:46:41 +0600

    fix: remove passport chase local authentication

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,38 +1,8 @@
 /* eslint-disable no-undef */
-
-const LocalStrategy = require("passport-local").Strategy;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const bcrypt = require("bcryptjs");
 const User = require("../models/user.model/user.model");
 // Passport Local Strategy
 module.exports = function (passport) {
-  // Local Strategy
-  passport.use(
-    "local",
-    new LocalStrategy(
-      { usernameField: "email" },
-      async (email, password, done) => {
-        try {
-          const user = await User.findOne({ email });
-          if (!user) {
-            return done(null, false, { message: "User not found" });
-          }
-
-          const isMatch = await bcrypt.compare(password, user.password);
-          if (!isMatch) {
-            return done(null, false, {
-              message: "Invalid Credentials Email/Password",
-            });
-          }
-
-          return done(null, user);
-        } catch (error) {
-          return done(error);
-        }
-      }
-    )
-  );
-
   // Google Strategy
   passport.use(
     new GoogleStrategy(
