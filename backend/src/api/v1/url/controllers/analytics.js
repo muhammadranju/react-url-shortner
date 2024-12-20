00f5b2000b67c637fd120b3fda115944d0d5ad6b
Muhammad Ranju <mdranju23@gmail.com>
2024-12-20 15:11:25 +0600

    feat: update Analytic page

--- backend/src/api/v1/url/controllers/analytics.js
+++ backend/src/api/v1/url/controllers/analytics.js
@@ -1,27 +1,16 @@
-const mongoose = require("mongoose");
 const ShortUrl = require("../../../../models/url.model/url.model");
 
 const analytics = async (req, res) => {
   try {
     const { id } = req.params;
 
-    let findShortUrl;
+    const findShortUrl = await ShortUrl.findOne({ shotLink: id });
 
-    if (mongoose.Types.ObjectId.isValid(id)) {
-      findShortUrl = await ShortUrl.findOne({
-        $or: [{ _id: id }],
-      });
-    }
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      findShortUrl = await ShortUrl.findOne({
-        $or: [{ shotLink: id }],
-      });
-    }
-    console.log(findShortUrl);
     res.status(200).json({
       status: 200,
       success: true,
       message: "Short URL Analytics",
+      analytics: findShortUrl,
     });
   } catch (error) {
     console.log(error);
